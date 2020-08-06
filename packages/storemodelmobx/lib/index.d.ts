ada9536d7a47965ee9ba2b796ad7fa543b993c3c
Nathan <nquinn@medonesystems.com>
2020-08-06 12:04:20 -0400

    update

--- packages/storemodelmobx/lib/index.d.ts
+++ packages/storemodelmobx/lib/index.d.ts
@@ -1,2 +1,5 @@
-export declare function start(): string;
+import { Store } from './base.mobx';
+import { Model } from './base.model';
+import { Service } from './service';
+export { Store, Model, Service };
 //# sourceMappingURL=index.d.ts.map
